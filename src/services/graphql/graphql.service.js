90173d1426b144825affb27309b469c7a9e731bf
3kynox <thierry.prost@gmail.com>
2017-10-15 19:21:12 +0200

    Cleanup

--- src/services/graphql/graphql.service.js
+++ src/services/graphql/graphql.service.js
@@ -1,30 +1,3 @@
-// Initializes the `graphql` service on path `/graphql`
-// const createService = require('./graphql.class.js');
-// const hooks = require('./graphql.hooks');
-// const filters = require('./graphql.filters');
-
-// module.exports = function () {
-//   const app = this;
-//   const paginate = app.get('paginate');
-
-//  const options = {
-//     name: 'graphql',
-//     paginate
-//   };
-
-  // Initialize our service with any options it requires
-//   app.use('/graphql', createService(options));
-
-  // Get our initialized service so that we can register hooks and filters
-//   const service = app.service('graphql');
-
-//   service.hooks(hooks);
-
-//   if (service.filter) {
-//     service.filter(filters);
-//   }
-// };
-
 const graphql = require('graphql-server-express').graphqlExpress;
 const graphiql = require('graphql-server-express').graphiqlExpress;
 const makeExecutableSchema = require('graphql-tools').makeExecutableSchema;
