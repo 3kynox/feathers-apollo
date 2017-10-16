d32caace0e6d02879bedf5e1ba9c2d66c856301f
3kynox <thierry.prost@gmail.com>
2017-10-17 00:39:59 +0200

    More on authentication and main app.js #2

---
+++ src/authentication.js
@@ -0,0 +1,35 @@
+const authentication = require('feathers-authentication');
+const jwt = require('feathers-authentication-jwt');
+const local = require('feathers-authentication-local');
+
+module.exports = function () {
+    const app = this;
+    const config = app.get('authentication');
+
+    // Adding swagger to the authentication
+    app.configure(authentication(config));
+
+    // delete app.docs.paths['/authentication/{id}'];
+    // app.docs.paths['/authentication'].post = Object.assign(
+    //     {},
+    //     app.docs.paths['/authentication'].post, def.post);
+    // app.docs.tags[0].description = def.description;
+
+    // Set up authentication with the secret
+    app.configure(jwt());
+    app.configure(local(config.local));
+
+    // The `authentication` service is used to create a JWT.
+    // The before `create` hook registers strategies that can be used
+    // to create a new valid JWT (e.g. local or oauth2)
+    app.service('authentication').hooks({
+        before: {
+            create: [
+                authentication.hooks.authenticate(config.strategies)
+            ],
+            remove: [
+                authentication.hooks.authenticate('jwt')
+            ]
+        }
+    });
+};
